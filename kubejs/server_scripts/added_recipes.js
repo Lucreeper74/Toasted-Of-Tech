af58087f8f28fcab4007ef891882b1ec807ac35f
Luc_Creeper74 <85123779+Lucreeper74@users.noreply.github.com>
2024-02-22 19:20:20 +0100

    Added custom recipe
    Mob Masher from MobGrindingUtils

--- kubejs/server_scripts/added_recipes.js
+++ kubejs/server_scripts/added_recipes.js
@@ -8,12 +8,24 @@ ServerEvents.recipes(e => {
     }).id(`kjs:crafting/thermal/cured_rubber`)
 
     e.shaped('waterstrainer:string_mesh', [
-      ' S ',
-      'SSS',
-      ' S '
-    ], {
-      S: 'minecraft:string'
-  }).id(`kjs:crafting/waterstrainer/string_mesh`)
+        ' S ',
+        'SSS',
+        ' S '
+      ], {
+        S: 'minecraft:string'
+    }).id(`kjs:crafting/waterstrainer/string_mesh`)
 
-  e.blasting('biggerreactors:graphite_ingot', 'createmetallurgy:graphite')
+    e.shaped('mob_grinding_utils:saw', [
+        'SDS',
+        'CRC',
+        'DID'
+      ], {
+        S: 'minecraft:iron_sword',
+        D: 'minecraft:diamond',
+        C: 'create:crushing_wheel',
+        R: 'minecraft:redstone_block',
+        I: 'minecraft:iron_block',
+    }).id(`kjs:crafting/mob_grinding_utils/saw`)
+
+    e.blasting('biggerreactors:graphite_ingot', 'createmetallurgy:graphite')
 })
