1a0739ba099de9d3e73c3cf8be51d1325140a9e6
Luc_Creeper74 <85123779+Lucreeper74@users.noreply.github.com>
2024-02-22 19:37:06 +0100

    Fix forge tag

--- kubejs/server_scripts/create/spout_recipes.js
+++ kubejs/server_scripts/create/spout_recipes.js
@@ -1,3 +1,3 @@
 ServerEvents.recipes(e => {
-    e.recipes.create.filling('minecraft:honeycomb', [Fluid.of('#forge:honey', 250), 'productivebees:honey_treat'])
+    e.recipes.create.filling('minecraft:honeycomb', [Fluid.of('forge:honey', 250), 'productivebees:honey_treat'])
 })
